30349eda659f6fddc11bf424a3fce63f9e74bac4
USER <C02TVKS8HV27@C02TVKS8HV27.local>
2017-08-08 18:42:16 +0800

    Adding API sign up and login Ill organizethe code later

---
+++ database/migrations/2017_08_08_032528_add_field_to_users.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->nullable();
+            $table->integer('role_id')->nullable();
+            $table->string('platform')->nullable();
+            $table->string('mobile_no')->nullable();
+            $table->string('business_name')->nullable();
+            $table->string('nric_no')->unique()->nullable();
+            $table->string('school')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
