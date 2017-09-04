c310483f95d135c0e8daf8d66d17c5d36e54637a
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-09-04 20:42:29 +0800

    Removing middleware

--- routes/api.php
+++ routes/api.php
@@ -66,7 +66,7 @@ Route::prefix('v1/')->group(function () {
 
 });
 
-Route::group(['middleware' => ['auth_client', 'auth:api']], function() {
+Route::group(['middleware' => ['auth_client']], function() {
 
     Route::get('v1/job/lists', 'Job\JobController@jobApiLists');
 
