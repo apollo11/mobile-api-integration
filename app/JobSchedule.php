710ad3c769dbaf64e60ad2e2c7c599c47d3c97f1
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-09-11 19:17:22 +0800

    Adding new file for Schedule API and Apply API

---
+++ app/JobSchedule.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
+
+class JobSchedule extends Model
+{
+    protected $fillable = [
+        'name'
+        , 'job_id'
+    ];
+
+    /**
+     * The table associated with the model
+     */
+    protected $table = 'job_schedules';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Implement has many through relationship related to jobs nd users
+     */
+    public function jobScheduleLists()
+    {
+        return $this->belongsTo('\App\User');
+    }
+
+    /**
+     * Implementation of job schedule via user
+     */
+    public function getJobByUserSchedule(array $param)
+    {
+        $jobs = DB::table('users')
+            ->join('job_schedules', 'job_schedules.user_id', '=', 'users.id')
+            ->join('jobs', 'jobs.id', '=', 'job_schedules.job_id')
+            ->select(
+                'jobs.id'
+                , 'job_schedules.user_id'
+                , 'job_schedules.job_id'
+                , 'users.company_description'
+                , 'users.company_name'
+                , 'jobs.description as job_description'
+                , 'jobs.location'
+                , 'jobs.location_id'
+                , 'jobs.industry'
+                , 'jobs.industry_id'
+                , 'jobs.job_date as start_date'
+                , 'jobs.created_at'
+                , 'jobs.end_date'
+                , 'jobs.contact_no'
+                , 'jobs.rate'
+                , 'jobs.job_image_path'
+                , 'jobs.nationality'
+                , 'jobs.choices as gender'
+                , 'jobs.description'
+                , 'jobs.min_age'
+                , 'jobs.max_age'
+                , 'jobs.role'
+                , 'jobs.notes'
+                , 'jobs.language'
+                , 'jobs.choices'
+                ,'jobs.job_requirements'
+            )
+            ->when(!empty($param['industries']), function ($query) use ($param) {
+
+                return $query->whereIn('jobs.industry_id', $param['industries']);
+            })
+            ->when(!empty($param['locations']), function ($query) use ($param) {
+
+                return $query->whereIn('jobs.location_id', $param['locations']);
+            })
+            ->when(!empty($param['start']) && !empty($param['created']), function ($query) use ($param) {
+
+                return $query->whereRaw("CASE WHEN jobs.job_date = '" . $param['start'] .
+                    "' THEN jobs.created_at < '" . $param['created'] .
+                    "' ELSE jobs.job_date <= '" . $param['start'] . "' END");
+
+            })
+            ->when(!empty($param['id']), function ($query) use ($param) {
+
+                $query->where('users.id', '=', $param['id']);
+            })
+            ->when(empty($param['limit']), function ($query) use ($param) {
+
+                $query->limit(20);
+            })
+            ->orderBy('jobs.job_date', 'desc')
+            ->orderBy('jobs.created_at', 'desc')
+            ->get();
+
+        return $jobs;
+    }
+
+    /**
+     * Implementation of job schedule via user
+     */
+    public function getJobScheduleDetails($id)
+    {
+        $jobs = DB::table('users')
+            ->join('job_schedules', 'job_schedules.user_id', '=', 'users.id')
+            ->join('jobs', 'jobs.id', '=', 'job_schedules.job_id')
+            ->select(
+                'jobs.id'
+                , 'job_schedules.user_id'
+                , 'job_schedules.job_id'
+                , 'users.company_description'
+                , 'users.company_name'
+                , 'jobs.description as job_description'
+                , 'jobs.location'
+                , 'jobs.location_id'
+                , 'jobs.industry'
+                , 'jobs.industry_id'
+                , 'jobs.job_date as start_date'
+                , 'jobs.created_at'
+                , 'jobs.end_date'
+                , 'jobs.contact_no'
+                , 'jobs.rate'
+                , 'jobs.job_image_path'
+                , 'jobs.nationality'
+                , 'jobs.choices as gender'
+                , 'jobs.description'
+                , 'jobs.min_age'
+                , 'jobs.max_age'
+                , 'jobs.role'
+                , 'jobs.notes'
+                , 'jobs.language'
+                , 'jobs.choices'
+                ,'jobs.job_requirements'
+            )
+            ->where('jobs.id', '=', $id)
+            ->first();
+
+        return $jobs;
+    }
+
+
+}
---
+++ database/migrations/2017_09_08_074423_add_new_user_field.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewUserField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->date('date_of_birth')->nullable();
+            $table->boolean('is_approved')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
