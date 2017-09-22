463d52935397781b5e9acaf013abbe6f921898fb
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-09-22 13:28:25 +0800

    Adjust query

--- app/Employee.php
+++ app/Employee.php
@@ -19,20 +19,23 @@ class Employee extends Model
     public function employeeLists()
     {
         $employee = DB::table('users')
-            ->join('job_schedules', 'job_schedules.user_id', '=', 'users.id')
-            ->join('jobs', 'jobs.id', '=', 'job_schedules.job_id')
-            ->join('users as employer', 'employer.id', '=', 'jobs.user_id')
+//            ->leftJoin('job_schedules', 'job_schedules.user_id', '=', 'users.id')
+//            ->leftJoin('job_schedules', function ($join) use ($userId) {
+//                $join->on('job_schedules.job_id', '=', 'jobs.id')
+//                    ->where('job_schedules.user_id', '=', $userId);
+//            })
+
             ->select(
-                    'users.id'
+                  'users.id'
                 , 'users.name'
                 , 'users.nric_no'
                 , 'users.mobile_no'
                 , 'users.date_of_birth'
                 , 'users.employee_status'
-                , 'employer.business_manager'
-                , 'employer.employee_status as status'
+                , 'users.business_manager'
+                , 'users.employee_status as status'
             )
-            ->where('job_schedules.job_status', '=', 'accepted')
+            ->where('users.role_id', '=', 2)
             ->orderBy('users.id', 'asc')
             ->distinct()
             ->get();
