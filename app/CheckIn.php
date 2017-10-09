d85cb258148666ceabec1098db14620741739753
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-10-09 18:58:23 +0800

    Update condition

--- app/CheckIn.php
+++ app/CheckIn.php
@@ -64,13 +64,13 @@ class CheckIn extends Model
             ->where('job_schedules.user_id', '=', $param['id'])
             ->where('job_schedules.job_status', '=', 'accepted')
             ->where(function ($query) {
+//                $query->where(function ($query) {
+//                    $query->whereNotNull('job_schedules.checkin_datetime');
+//                    $query->whereNull('job_schedules.checkout_datetime');
+//
+//                });
+
                 $query->where(function ($query) {
-                    $query->whereNotNull('job_schedules.checkin_datetime');
-                    $query->whereNull('job_schedules.checkout_datetime');
-
-                });
-
-                $query->orWhere(function ($query) {
                     $query->whereNull('job_schedules.checkin_datetime');
                     $query->whereRaw("jobs.job_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 DAY ) AND NOW()");
 
