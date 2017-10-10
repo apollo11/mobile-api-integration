ba906b991bdd70e68407a2f93ae2fe064e7523a4
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-10-10 12:50:06 +0800

    Schema for notification

--- bootstrap/constant.php
+++ bootstrap/constant.php
@@ -4,4 +4,3 @@ define('GOOGLE_ENDPOINT','https://www.googleapis.com');
 define('OUATH_ENDPOINT', 'http://13.58.45.200/oauth/token');
 define('GOOGLE_MAP_ENDPOINT', 'https://maps.googleapis.com/maps/api/geocode/json');
 //define('OUATH_ENDPOINT', 'http://yyjobs.local/oauth/token');
-
