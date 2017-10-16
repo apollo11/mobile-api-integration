ef5578c23e5109fba86139970dab555b37d94289
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-10-16 22:42:35 +0800

    Changes employee

--- resources/views/employee/lists.blade.php
+++ resources/views/employee/lists.blade.php
@@ -118,6 +118,16 @@
                                                     </button>
                                                     <ul class="dropdown-menu" role="menu">
                                                         <li>
+                                                            <a href="{{ route('logout') }}"
+                                                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                                                Logout
+                                                            </a>
+
+                                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                                {{ csrf_field() }}
+                                                            </form>
+
                                                             <a href="javascript:;">
                                                                 <i class="fa fa-trash"></i> Delete</a>
                                                         </li>
