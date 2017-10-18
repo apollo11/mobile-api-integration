c4da4c22a6606367f6d209e1ed5b2f06d383006f
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-10-18 14:46:38 +0800

    Updating styles for jobs

--- resources/views/job/lists.blade.php
+++ resources/views/job/lists.blade.php
@@ -1,90 +1,113 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="page-content-wrapper">
+    <div class="page-content-wrapper employee-list">
         <div class="page-content">
-            <div class="row">
-                <div class="form-group float-left">
-                    <div class="col-md-4">
-                        <div class="input-group">
-                            <div class="input-icon right">
-                                <i class="fa fa-search"></i>
-                                <input type="text" class="form-control" placeholder="Search">
+            <form action="{{ route('employee.destroy-all')  }}" method="POST">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="portlet light bordered">
+                            <div class="portlet-title">
+                                <div class="caption font-dark">
+                                    <i class="icon-settings font-dark"></i>
+                                    <span class="caption-subject bold uppercase">list of Jobs</span>
+                                </div>
+                                {{ csrf_field() }}
+                                <div class="actions">
+                                    <input class="btn sbold green" name="multiple" value="Approve" type="submit"/>
+                                    <input class="btn sbold green" name="multiple" value="Delete" type="submit"/>
+                                    <a href="" id="sample_editable_1_new"
+                                       class="btn sbold green"> Add New
+                                        <i class="fa fa-plus"></i>
+                                    </a>
+
+                                </div>
                             </div>
-                            <span class="input-group-btn">
-                            <button class="btn blue left">Advance Filter</button>
-                        </span>
-                        </div>
-                    </div>
-                    <div class="col-md-3 col-md-offset-5 float-right">
-                        <a class="btn blue right">Delete</a>
-                        <a class="btn blue right">Export</a>
-                        <a href="{{ route('job.create') }}" class="btn blue right">Add New Post</a>
-                    </div>
-                </div>
-            </div>
-            <br />
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="portlet box green">
-                        <div class="portlet-title">
-                            <div class="caption">
-                                <i class="fa fa-comments"></i>Jobs
-                            </div>
-                        </div>
-                        <div class="portlet-body">
-                            <div class="table-scrollable">
-                                <table class="table table-striped table-hover">
+                            <div class="portlet-body">
+                                <table class="table table-striped table-bordered table-hover table-checkable order-column" id="employee-table">
                                     <thead>
                                     <tr>
-                                        <ap></ap>
+                                        <th>
+                                            <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                                <input type="checkbox" class="group-checkable"
+                                                       data-set="#employee-table .checkboxes"/>
+                                                <span></span>
+                                            </label>
+                                        </th>
                                         <th>#</th>
                                         <th>Employer</th>
-                                        <th>Job Title</th>
-                                        <th>Required</th>
-                                        <th>Applied</th>
-                                        <th>Job Description</th>
+                                        <th>Job Name</th>
+                                        <th> Employees Required</th>
+                                        <th>Employees Applied</th>
                                         <th>Rate</th>
-                                        <th> Job Date</th>
-                                        <th> Job Time</th>
+                                        <th> Job Date & Time</th>
+                                        <th>Business Manager</th>
+                                        <th> Job Location</th>
                                         <th> Status</th>
                                         <th> Action</th>
                                     </tr>
                                     </thead>
                                     <tbody>
                                     @foreach($job as $value)
-                                        <tr>
-                                            <td>{{ $value->id }}</td>
-                                            <td>{{ $value->company_name }}</td>
-                                            <td>{{ $value->job_title }}</td>
-                                            <td>{{ $value->no_of_person }}</td>
-                                            <td>{{ '0' }}</td>
-                                            <td>{{ str_limit($value->job_description, 15) }}</td>
-                                            <td> {{ '$'.$value->rate.'/hr' }}</td>
-                                            <td> {{ Carbon\Carbon::parse($value->start_date)->format('d-m-Y') }}</td>
-                                            <td> {{ Carbon\Carbon::parse($value->start_date)->format('H:i:s') }}
-                                                - {{ Carbon\Carbon::parse($value->end_date)->format('H:i:s') }}</td>
-                                            <td> {{ $value->status }}</td>
-                                            <td>
-                                                <a href="#">
-                                                    <i class="fa fa-edit"></i>
-                                                </a>
-                                                <a href="#">
-                                                    <i class="fa fa-eye"></i>
-                                                </a>
-                                                <a href="#">
-                                                    <i class="fa fa-trash"></i>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                    <tr class="odd gradeX">
+                                        <td>
+                                            <label class="mt-checkbox mt-checkbox-single mt-checkbox-outline">
+                                                <input type="checkbox" name="multicheck[]" class="checkboxes"
+                                                       value=""/>
+                                                <span></span>
+                                            </label>
+                                        </td>
+                                        <td>{{ $value->id }}</td>
+                                        <td>{{ $value->company_name }}</td>
+                                        <td>{{ $value->job_title }}</td>
+                                        <td>{{ $value->no_of_person }}</td>
+                                        <td>0</td>
+                                        <td> {{ '$'.$value->rate.'/hr' }}</td>
+                                        <td> {{ Carbon\Carbon::parse($value->start_date)->format('H:i:s d-m-Y') }}</td>
+                                        <td>Jorden Ng</td>
+                                        <td>Amora Road</td>
+                                        <td><span class="label label-sm label-warning"> {{ $value->status }} </span></td>
+                                        <td>
+                                            <div class="btn-group">
+                                                <button class="btn btn-xs green dropdown-toggle" type="button"
+                                                        data-toggle="dropdown" aria-expanded="false"> Actions
+                                                    <i class="fa fa-angle-down"></i>
+                                                </button>
+                                                <ul class="dropdown-menu" role="menu">
+                                                    <li>
+                                                        <a href=""
+                                                           onclick="">
+                                                            <i class="fa fa-trash"></i> Delete</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="">
+                                                            <i class="fa fa-edit"></i> Edit </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="">
+                                                            <i class="fa fa-eye"></i> View </a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="" onclick="">
+                                                            <i class="fa fa-check-square-o"></i> Approve</a>
+                                                    </li>
+                                                    <li>
+                                                        <a href="" onclick="">
+                                                            <i class="fa fa-close"></i> Reject
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 @endsection
