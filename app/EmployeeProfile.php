0e5af30e4824f7ee77ca56cd767c7dfe89583707
Apollo <apollo.melapote@monstar-lab.com.sg>
2017-11-02 12:33:06 +0800

    Pending jobs lef join job

--- app/EmployeeProfile.php
+++ app/EmployeeProfile.php
@@ -35,8 +35,15 @@ class EmployeeProfile extends Model
     {
         $count = DB::table('users')
             ->leftJoin('job_schedules', 'job_schedules.user_id', '=', 'users.id')
+            ->leftJoin('jobs', 'jobs.id', '=', 'job_schedules.job_id')
             ->where('users.id', '=',  $userid)
            ->where('job_schedules.job_status', '=', 'accepted')
+            ->where(function ($query) {
+                // Job must have no check in time
+                $query->whereNull('job_schedules.checkin_datetime');
+                // Checking in is allow as long as the the job's end date has not ended yet
+                $query->where('jobs.end_date', '>', Carbon::now());
+            })
             ->count();
 
         return $count;
