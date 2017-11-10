d69b05d66a2cd28e164d24f52f051a7a790a73c7
Miya <amopopo@gmail.com>
2017-11-10 14:28:53 +0800

    remove comment

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -3,19 +3,6 @@
 @section('content')
     <div class="page-content-wrapper">
         <div class="page-content">
-<!-- 
-            <div class="page-bar">
-                <ul class="page-breadcrumb">
-                    <li>
-                        <a href="{{ route('industry.lists')  }}">Industry</a>
-                        <i class="fa fa-circle"></i>
-                    </li>
-                    <li>
-                        <span>Add Industry</span>
-                    </li>
-                </ul>
-            </div>
- -->
             <div class="row">
                 <div class="col-md-8">
                     <div class="portlet light bordered">
