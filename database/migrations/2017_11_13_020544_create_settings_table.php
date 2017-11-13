ccf5690b60ab19d273090d066e3a8f2cf45eb98c
Miya <amopopo@gmail.com>
2017-11-13 10:19:40 +0800

    add settings page

---
+++ database/migrations/2017_11_13_020544_create_settings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('terms_conditions');
+            $table->text('privacy_policy');
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
