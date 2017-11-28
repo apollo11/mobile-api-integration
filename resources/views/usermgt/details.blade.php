885149c81595ba5fb4b2ef0ae3d25aec46196956
Ashwin Kurlye <ashwin.kurlye@monstar-lab.com.sg>
2017-11-28 14:26:29 +0800

    User Management -> Action -> View Fix

--- resources/views/usermgt/details.blade.php
+++ resources/views/usermgt/details.blade.php
@@ -6,7 +6,7 @@
             <div class="page-bar">
                 <ul class="page-breadcrumb">
                     <li>
-                        <a href="{{ route('mgt.lists')  }}">Employers</a>
+                        <a href="{{ route('mgt.list')  }}">User Management</a>
                         <i class="fa fa-circle"></i>
                     </li>
                     <li>
