2db40e49627d67814eda16a52c5eb97c906ec798
Miya <amopopo@gmail.com>
2017-11-28 14:10:02 +0800

    fix logged in but when click to home link it shows login page with sidebar issue

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('auth.login');
-});
+});*/
 
 Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
@@ -156,6 +156,7 @@ Route::group(['middleware' => ['auth']], function () {
 
 });
 
+Route::get('/', 'HomeController@index')->middleware('can:dashboard-view');
 Route::get('/home', 'HomeController@index')->name('home')->middleware('can:dashboard-view');
 
 /*content page for mobile app*/
