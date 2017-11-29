b424581a79105570f85c17c8b3a5bca8ae93b448
Miya <amopopo@gmail.com>
2017-11-29 14:09:53 +0800

    change link in dashboard and temp fix bug for assign list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,95 +3,8 @@
 @section('content')
     <div class="page-content-wrapper">
         <div class="page-content">
-
             <!-- BEGIN PAGE HEADER-->
-            <!-- BEGIN THEME PANEL -->
-            <div class="theme-panel hidden-xs hidden-sm">
-                <div class="toggler-close"></div>
-                <div class="theme-options">
-                    <div class="theme-option theme-colors clearfix">
-                        <span> THEME COLOR </span>
-                        <ul>
-                            <li class="color-default current tooltips" data-style="default" data-container="body"
-                                data-original-title="Default"></li>
-                            <li class="color-darkblue tooltips" data-style="darkblue" data-container="body"
-                                data-original-title="Dark Blue"></li>
-                            <li class="color-blue tooltips" data-style="blue" data-container="body"
-                                data-original-title="Blue"></li>
-                            <li class="color-grey tooltips" data-style="grey" data-container="body"
-                                data-original-title="Grey"></li>
-                            <li class="color-light tooltips" data-style="light" data-container="body"
-                                data-original-title="Light"></li>
-                            <li class="color-light2 tooltips" data-style="light2" data-container="body" data-html="true"
-                                data-original-title="Light 2"></li>
-                        </ul>
-                    </div>
-                    <div class="theme-option">
-                        <span> Theme Style </span>
-                        <select class="layout-style-option form-control input-sm">
-                            <option value="square" selected="selected">Square corners</option>
-                            <option value="rounded">Rounded corners</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Layout </span>
-                        <select class="layout-option form-control input-sm">
-                            <option value="fluid" selected="selected">Fluid</option>
-                            <option value="boxed">Boxed</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Header </span>
-                        <select class="page-header-option form-control input-sm">
-                            <option value="fixed" selected="selected">Fixed</option>
-                            <option value="default">Default</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Top Menu Dropdown</span>
-                        <select class="page-header-top-dropdown-style-option form-control input-sm">
-                            <option value="light" selected="selected">Light</option>
-                            <option value="dark">Dark</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Sidebar Mode</span>
-                        <select class="sidebar-option form-control input-sm">
-                            <option value="fixed">Fixed</option>
-                            <option value="default" selected="selected">Default</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Sidebar Menu </span>
-                        <select class="sidebar-menu-option form-control input-sm">
-                            <option value="accordion" selected="selected">Accordion</option>
-                            <option value="hover">Hover</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Sidebar Style </span>
-                        <select class="sidebar-style-option form-control input-sm">
-                            <option value="default" selected="selected">Default</option>
-                            <option value="light">Light</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Sidebar Position </span>
-                        <select class="sidebar-pos-option form-control input-sm">
-                            <option value="left" selected="selected">Left</option>
-                            <option value="right">Right</option>
-                        </select>
-                    </div>
-                    <div class="theme-option">
-                        <span> Footer </span>
-                        <select class="page-footer-option form-control input-sm">
-                            <option value="fixed">Fixed</option>
-                            <option value="default" selected="selected">Default</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <!-- END THEME PANEL -->
+          
             <!-- BEGIN PAGE BAR -->
             <div class="page-bar">
                 <ul class="page-breadcrumb">
@@ -133,7 +46,7 @@
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <a href="/job/lists?status=active" class="dashboard-stat dashboard-stat-v2 red" href="#">
+                    <a href="{{ route('job.lists') }}?status=active" class="dashboard-stat dashboard-stat-v2 red" href="#">
                         <div class="visual">
                             <i class="fa fa-comments"></i>
                         </div>
@@ -159,7 +72,7 @@
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <a class="dashboard-stat dashboard-stat-v2 purple" href="/payout/lists?payment-status=pending">
+                    <a class="dashboard-stat dashboard-stat-v2 purple" href="{{ route('payout.lists') }}?payment-status=pending">
                         <div class="visual">
                             <i class="fa fa-comments"></i>
                         </div>
@@ -173,7 +86,7 @@
                 </div>
 
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <a href="/employee/lists?checkin=true" class="dashboard-stat dashboard-stat-v2 blue" href="#">
+                    <a href="{{ route('employee.lists') }}?checkin=true" class="dashboard-stat dashboard-stat-v2 blue" href="#">
                         <div class="visual">
                             <i class="fa fa-comments"></i>
                         </div>
@@ -186,7 +99,7 @@
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <a href="/employee/lists?checkout=true" class="dashboard-stat dashboard-stat-v2 red" >
+                    <a href="{{ route('employee.lists') }}?checkout=true" class="dashboard-stat dashboard-stat-v2 red" >
                         <div class="visual">
                             <i class="fa fa-comments"></i>
                         </div>
@@ -199,7 +112,7 @@
                     </a>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <a href="/employee/lists?status=cancelled" class="dashboard-stat dashboard-stat-v2 green" href="#">
+                    <a href="{{ route('employee.lists') }}?status=cancelled" class="dashboard-stat dashboard-stat-v2 green" href="#">
                         <div class="visual">
                             <i class="fa fa-comments"></i>
                         </div>
